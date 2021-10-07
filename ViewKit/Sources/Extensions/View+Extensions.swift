52ebdd3a782b8b3ddbebd87d45471dedbf33a85e
Christoffer Winterkvist <christoffer@winterkvist.com>
2021-10-07 22:06:52 +0200

    Fix bug where multiple workflows are created when dragging files into the list

--- ViewKit/Sources/Extensions/View+Extensions.swift
+++ ViewKit/Sources/Extensions/View+Extensions.swift
@@ -49,40 +49,27 @@ public extension View {
       isTargeted: isTargeted,
       perform: { providers in
         var urls = Set<URL>()
-        var counter = 0
-        let completion = {
-          counter -= 1
-          if counter == 0 {
-            DispatchQueue.main.async {
-              handler(Array(urls))
-            }
-          }
-        }
-
         for provider in providers {
           // Try and decode URL's
           if provider.canLoadObject(ofClass: URL.self) {
-            counter += 1
             _ = provider.loadObject(ofClass: URL.self) { url, _ in
               if let newUrl = url {
                 urls.insert(newUrl)
               }
-              completion()
             }
           }
 
           // Try and decode Strings
           if provider.canLoadObject(ofClass: String.self) {
-            counter += 1
             _ = provider.loadObject(ofClass: String.self) { string, _ in
               if let string = string,
                  let url = URL(string: string) {
                 urls.insert(url)
               }
-              completion()
             }
           }
         }
+        handler(Array(urls))
         return true
       })
   }
